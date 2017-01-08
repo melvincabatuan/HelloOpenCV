9f5e232c04cec12c6bd9e6d0709e52a1ed26d49e
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2017-01-08 13:11:35 +0800

    Create OpenCV_Version.cpp

---
+++ OpenCV_Version.cpp
@@ -0,0 +1,16 @@
+/*
+ * OpenCV_Version.cpp
+ *
+ *  Created on: Jan 8, 2017
+ *      Author: cobalt
+ */
+
+
+#include <iostream>
+#include <opencv2/core.hpp>  
+
+int main(int argc, char *argv[])
+{
+	std::cout<<"OpenCV version: "<< CV_VERSION << std::endl;
+	return 0;
+}
